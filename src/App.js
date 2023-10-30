29b4e99e0ed8b8b1d223617047c812cb2901e7c9
Dhanush-Mohan <dhanish4777@gmail.com>
2023-10-30 15:57:21 +0530

    Skills.js added
    Added the first commit of skills.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/NavBar/Navbar";
 import Intro from "./components/Intro/Intro";
+import Skills from "./components/Skills/Skills";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
       <Intro></Intro>
+      <Skills></Skills>
     </div>
   );
 }
---
+++ src/components/Skills/utils.js
@@ -0,0 +1,91 @@
+export const skillsData = [
+    {
+        label: "FRONTEND",
+        data: [
+            {
+                skillName: "HTML",
+                percentage: "80",
+            },
+            {
+                skillName: "CSS",
+                percentage: "60",
+            },
+            {
+                skillName: "JAVASCRIPT",
+                percentage: "70",
+            },
+            {
+                skillName: "REACT JS",
+                percentage: "50",
+            },
+            {
+                skillName: "JAVASCRIPT",
+                percentage: "70",
+            },
+
+           
+        ],
+    },
+    {
+        label: "BACKEND",
+        data: [
+            {
+                skillName: "PYTHON",
+                percentage: "80",
+            },
+            {
+                skillName: "JAVA",
+                percentage: "40",
+            },
+            {
+                skillName: "NODE JS",
+                percentage: "40",
+            },
+            {
+                skillName: "EXPRESS JS",
+                percentage: "50",
+            },
+           
+        ],
+    },
+    {
+        label: "DATABASES",
+        data: [
+            {
+                skillName: "MYSQL",
+                percentage: "90",
+            },
+            {
+                skillName: "MONGO DB",
+                percentage: "60",
+            },
+           
+        ],
+    },
+    {
+        label: "DATA SCIENCE",
+        data: [
+            {
+                skillName: "PYTHON",
+                percentage: "90",
+            },
+            {
+                skillName: "ML",
+                percentage: "80",
+            },
+            {
+                skillName: "DL",
+                percentage: "60",
+            },
+            {
+                skillName: "NLP",
+                percentage: "70",
+            },
+            {
+                skillName: "DATA ENGINEERING",
+                percentage: "40",
+            },
+           
+        ],
+    },
+]
