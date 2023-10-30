478c606aa61092404513c033cc17cc51fe096e29
Dhanush-Mohan <dhanish4777@gmail.com>
2023-10-30 09:23:31 +0530

    Intro.js completed

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./intro.css";
 import { Link } from "react-scroll";
-import bg from "../../assets/dp2.png";
-import resume from "../../assets/resume.svg"
-import "../../../node_modules/boxicons/css/boxicons.min.css"
+import bg from "../../assets/dp1.png";
+import resume from "../../assets/resume.svg";
+import "../../../node_modules/boxicons/css/boxicons.min.css";
 
 const Intro = () => {
   return (
@@ -13,28 +13,44 @@ const Intro = () => {
         <span className="introText">
           I'm <span className="introName">Dhanush Mohan</span>
           <br />
-          Python Developer
+          Fullstack Developer
         </span>
         <p className="introPara">
-          An enthusiastic programmer aspiring to explore new opportunities in
-          the field of Technology and Development keeping the learning process
-          on.
+          An enthusiastic programmer aspiring to explore new
+          <br />
+          opportunities in the field of Technology and Development
+          <br />
+          keeping the learning process on.
         </p>
         <div className="social">
-            <Link href="#" className="socialItem"><i className="bx bxl-linkedin"></i></Link>
-            <Link href="#" className="socialItem"><i className="bx bxl-github"></i></Link>
-            <Link href="#" className="socialItem"><i className="bx bxl-instagram"></i></Link>
-            <Link href="#" className="socialItem"><i className="bx bxl-whatsapp"></i></Link>
-            <Link href="#" className="socialItem"><i className="bx bxl-gmail"></i></Link>
+          <Link href="#" className="socialItem">
+            <i className="bx bxl-linkedin"></i>
+          </Link>
+          <Link href="#" className="socialItem">
+            <i className="bx bxl-github"></i>
+          </Link>
+          <Link href="#" className="socialItem">
+            <i className="bx bxl-instagram"></i>
+          </Link>
+          <Link href="#" className="socialItem">
+            <i className="bx bxl-whatsapp"></i>
+          </Link>
+          <Link href="#" className="socialItem">
+            <i className="bx bxl-gmail"></i>
+          </Link>
         </div>
-        <Link><button className="btn">
+        <Link>
+          <button className="btn">
             <div className="resumeDiv">
-                <img src={resume} alt="Resume" className="resume" />
-                <p>Resume</p>
+              <img src={resume} alt="Resume" className="resume" />
+              <p>Resume</p>
             </div>
-            </button></Link>
+          </button>
+        </Link>
       </div>
-      <img src={bg} alt="" className="bg" />
+      <div className="introdp">
+        <img src={bg} alt="" className="bg" />
+      </div>
     </section>
   );
 };
