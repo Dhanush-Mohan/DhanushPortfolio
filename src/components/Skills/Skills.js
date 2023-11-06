3d6e42f12f3bf698b4f30400474b5c61dfe6cd76
Dhanush-Mohan <dhanish4777@gmail.com>
2023-11-06 13:25:49 +0530

    Update skills.css
    Added max-width:75vw

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,56 +1,60 @@
 import React from "react";
 import { skillsData } from "./utils";
-import { Line } from 'rc-progress';
+import { Line } from "rc-progress";
 import { Animate, AnimateKeyframes } from "react-simple-animate";
-import './skills.css';
+import "./skills.css";
 
 const Skills = () => {
   return (
-    <section id="skills" className="skills">
-      <span className="skillTitle">Skills</span>
-      <div className="skills__content-wrapper">
-        {skillsData.map((item, i) => (
-          <div key={i} className="skills__content-wrapper__inner-content">
-            <Animate
-              play
-              duration={1}
-              delay={0.3}
-              start={{
-                transform: "translateX(-200px)",
-              }}
-              end={{
-                transform: "translateX(0px)",
-              }}
-            >
-              <h3 className="skills__content-wrapper__inner-content__category-text">
-                {item.label}
-              </h3>
-              <div className="maindiv">
-                {item.data.map((skillItem, j) => (
-                  <AnimateKeyframes
-                    play
-                    duration
-                    keyframes={["opacity:1", "opacity:0"]}
-                    iterationCount="1"
-                  >
-                    <div className="progressbar-wrapper" key={j}>
-                      <p className="progressbar-wrapper-p">{skillItem.skillName}</p>
-                      <Line
-                        percent={skillItem.percentage}
-                        strokeWidth="4"
-                        strokeColor="gold"
-                        trailWidth={"4"}
-                        strokeLinecap="square"
-                      ></Line>
-                    </div>
-                  </AnimateKeyframes>
-                ))}
-              </div>
-            </Animate>
-          </div>
-        ))}
-      </div>
-    </section>
+    <div className="main">
+      <section id="skills" className="skills">
+        <span className="skillTitle">Skills</span>
+        <div className="skills__content-wrapper">
+          {skillsData.map((item, i) => (
+            <div key={i} className="skills__content-wrapper__inner-content">
+              <Animate
+                play
+                duration={1}
+                delay={0.3}
+                start={{
+                  transform: "translateX(-200px)",
+                }}
+                end={{
+                  transform: "translateX(0px)",
+                }}
+              >
+                <h3 className="skills__content-wrapper__inner-content__category-text">
+                  {item.label}
+                </h3>
+                <div className="maindiv">
+                  {item.data.map((skillItem, j) => (
+                    <AnimateKeyframes
+                      play
+                      duration
+                      keyframes={["opacity:1", "opacity:0"]}
+                      iterationCount="1"
+                    >
+                      <div className="progressbar-wrapper" key={j}>
+                        <p className="progressbar-wrapper-p">
+                          {skillItem.skillName}
+                        </p>
+                        <Line
+                          percent={skillItem.percentage}
+                          strokeWidth="4"
+                          strokeColor="gold"
+                          trailWidth={"4"}
+                          strokeLinecap="square"
+                        ></Line>
+                      </div>
+                    </AnimateKeyframes>
+                  ))}
+                </div>
+              </Animate>
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
   );
 };
 
